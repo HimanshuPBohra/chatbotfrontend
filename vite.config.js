840ca4e9ce804289a149eb63c306a391af76188b
Himanshu Prakash Bohra <55129762+HimanshuPBohra@users.noreply.github.com>
2025-03-06 18:56:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "20.244.39.55",
+    port: 5012,  // Using alternate port since 5173 is in use
+    open: true,  // Automatically opens browser
+    strictPort: true  // Prevents conflicts
+  }
 });
