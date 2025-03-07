41fb2aca45c5cb789c2caabf6a781643b481df6b
Himanshu Prakash Bohra <55129762+HimanshuPBohra@users.noreply.github.com>
2025-03-07 12:29:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "https://parser.uknowva.cloud:5012",
+    host: "https://parser.uknowva.cloud",
     port: 5012,  // Using alternate port since 5173 is in use
     open: true,  // Automatically opens browser
     strictPort: true  // Prevents conflicts
